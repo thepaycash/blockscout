6b46682163397f26a115ec17314fde84641ed20b
Victor Baranov <baranov.viktor.27@gmail.com>
2019-12-08 18:36:58 +0300

    Disable uncataloged tt fetcher

--- apps/indexer/config/config.exs
+++ apps/indexer/config/config.exs
@@ -48,6 +48,7 @@ config :indexer,
 config :indexer, Indexer.Fetcher.ReplacedTransaction.Supervisor, disabled?: true
 # config :indexer, Indexer.Fetcher.BlockReward.Supervisor, disabled?: true
 config :indexer, Indexer.Fetcher.StakingPools.Supervisor, disabled?: true
+config :indexer, Indexer.Temporary.UncatalogedTokenTransfers.Supervisor, disabled?: true
 
 config :indexer, Indexer.Supervisor, enabled: System.get_env("DISABLE_INDEXER") != "true"
 
