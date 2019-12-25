a81f7e59be5cee60ad9699a033b4e65f6111bb5d
Victor Baranov <baranov.viktor.27@gmail.com>
2019-12-25 12:57:24 +0300

    Debug level for logs/prod/ethereum_jsonrpc.log

--- apps/ethereum_jsonrpc/config/prod.exs
+++ apps/ethereum_jsonrpc/config/prod.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :ethereum_jsonrpc, EthereumJSONRPC.Tracer, env: "production", disabled?: true
 
 config :logger, :ethereum_jsonrpc,
-  level: :info,
+  level: :debug,
   path: Path.absname("logs/prod/ethereum_jsonrpc.log"),
   rotate: %{max_bytes: 52_428_800, keep: 19}
